4ff0e6d7d29320886cc7f078465e2a4a0b65c377
Jongho Lee <bazinga940@gmail.com>
2024-10-28 23:02:14 -0400

    Fix API request issue

--- doctor-office-backend/server.js
+++ doctor-office-backend/server.js
@@ -2,17 +2,20 @@ const express = require("express");
 const app = express();
 require("dotenv").config();
 const dbConfig = require("./config/dbConfig");
+const cors = require("cors");
+const path = require("path");
+
+app.use(cors());
 app.use(express.json());
+
 const userRoute = require("./routes/userRoute");
 const adminRoute = require("./routes/adminRoute");
 const doctorRoute = require("./routes/doctorsRoute");
-const path = require("path");
-const cors = require("cors");
 
 app.use("/api/user", userRoute);
 app.use("/api/admin", adminRoute);
 app.use("/api/doctor", doctorRoute);
-app.use(cors());
+
 
 if (process.env.NODE_ENV === "production") {
   app.use("/", express.static("client/build"));
